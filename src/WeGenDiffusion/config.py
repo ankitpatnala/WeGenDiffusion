6e673cc99945d0fecc74c8257c40d13c5d86e5fc
Patnala,Ankit <a.patnala@fz-juelich.de>
2025-06-12 17:58:41 +0200

    updated filepath

--- src/WeGenDiffusion/config.py
+++ src/WeGenDiffusion/config.py
@@ -1,1 +1,2 @@
-input_filepath="/p/scratch/hclimrep/shared/wegen_hackathon/2021_t2m_era5_2deg.nc"
+train_filepath="/p/scratch/hclimrep/shared/wegen_hackathon/2011_t2m_era5_2deg.nc"
+val_filepath="/p/scratch/hclimrep/shared/wegen_hackathon/2012_t2m_era5_4months_2deg.nc"

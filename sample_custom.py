e836224ae28f157eeaf61139401783938c7e1481
Amaury Lancelin <amaury-lancelin@hotmail.fr>
2025-08-28 18:03:06 +0200

    new chkpts

---
+++ sample_custom.py
@@ -0,0 +1,62 @@
+import xarray as xr
+import torch
+import matplotlib.pyplot as plt
+import torch
+from models import DiT_models
+from diffusion import create_diffusion
+
+model_path = "/p/project1/training2533/lancelin1/WeGenDiffusion/results/DiT-B-2_previous_state/ckpt_0000040.pt"
+data_path = "/p/project1/training2533/lancelin1/WeGenDiffusion/data/2011_t2m_era5_2deg.nc"
+num_labels = 12
+shape = (num_labels,1,90,180)
+generate = True
+output_name = 'previous_state'
+axis_name = 'previous_state'
+combine_imgs = False
+
+if generate:
+    model_state_dict = torch.load(model_path, map_location='cuda')['model']
+    model = DiT_models['DiT-B/2'](input_size=(90,180),num_classes=1000).to('cuda')
+    model.load_state_dict(model_state_dict,strict=True)
+    model.eval()
+
+    diffusion = create_diffusion(timestep_respacing="")
+    y = torch.arange(0, shape[0],dtype=torch.long,device='cuda')
+
+    denoised_images = diffusion.p_sample_loop(model,shape,model_kwargs=dict(y=y))
+    # save the raw outputs since generation is expensive
+    torch.save(denoised_images, f'{output_name}_samples.pt')
+
+else:
+    denoised_images = torch.load(f'{output_name}_samples.pt')
+
+denoised_images = torch.squeeze(denoised_images, 1)
+
+ds = xr.open_dataset(data_path)
+mean = ds.mean()['t2m'].values
+std = ds.std()['t2m'].values
+
+samples = []
+for i in range(shape[0]):
+    samples.append(xr.Dataset({
+    "t2m": (("lat", "lon"), mean + std*denoised_images[i].cpu().numpy())
+    },
+    coords={
+        'lon':ds['lon'],
+         'lat':ds['lat']}))
+
+combined = xr.concat(samples,dim=axis_name)
+
+combined["t2m"].plot(
+    col=axis_name,
+    col_wrap=6,
+    cmap="viridis",
+    cbar_kwargs={
+        "orientation": "vertical",   # vertical colorbar at the side
+        "pad": 0.05,                  # distance from the plots
+        "shrink": 0.8                  # shrink length
+    },
+    vmin=220,
+    vmax=320
+)
+plt.savefig(f'{output_name}_samples.png')
--- sample_custom.sh
+++ sample_custom.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=dit_t2m
-#SBATCH --time=02:00:00
+#SBATCH --job-name=dit_sample
+#SBATCH --time=00:30:00
 #SBATCH --account=training2533
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --partition=dc-gpu
 #SBATCH --output=./logs/%x.%j.out
 #SBATCH --error=./logs/%x.%j.err
 #SBATCH --reservation=dc-gpu-devel
 
-#mlesm_hackathon_2
+#import modules and activate the virtual
 ml GCCcore/13.3.0 Python/3.12.3-GCCcore-13.3.0
 
 # environmental variables to support cpus_per_task with Slurm>22.05
@@ -19,15 +19,14 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export SRUN_CPUS_PER_TASK="${SLURM_CPUS_PER_TASK}"
 
 #activate virtual environment
+#source /fast/home/nishant.kumar/venv_python_3.10/bin/activate
 source .venv/bin/activate
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3,5,6,7"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 export NCCL_SOCKET_IFNAME=ib0
-export MASTER_PORT=12340
+export MASTER_PORT=12341
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=${master_addr}
 
-echo "Start time: $(date +%T)"
-srun --overlap python train.py --global-batch-size 4 --epochs=250 --labels=$1
-echo "End time: $(date +%T)"
-
+#run the python file
+srun --overlap python sample_custom.py
